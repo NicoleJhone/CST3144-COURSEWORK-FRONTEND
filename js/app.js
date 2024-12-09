6ad633ec94074f00a66629e4ff78cabe3055a3c1
NicoleJhone <105268456+NicoleJhone@users.noreply.github.com>
2024-12-09 19:12:33 +0400

    added sort functionality with asc/desc option

--- js/app.js
+++ js/app.js
@@ -1,7 +1,56 @@
 var webstore = new Vue({
     el: '#app',
     data: {
+        sortBy: 'subject',
+        ascending: true,
         sitename: 'After School Classes',
         subjects: subjects,
+    },
+    computed: {
+        sortedSubjects() {
+            let subjectsArray = this.subjects;
+
+            // sort by subjects
+            subjectsArray = subjectsArray.sort((a, b) => {
+                if (this.sortBy == 'subject') {
+                    let fa = a.title.toLowerCase(), fb = b.title.toLowerCase()
+                    if (fa < fb) {
+                        return -1
+                    }
+                    if (fa > fb) {
+                        return 1
+                    }
+                    return 0
+                } //sort by location
+                else if (this.sortBy == 'location') {
+                    let fa = a.location.toLowerCase(), fb = b.location.toLowerCase()
+                    if (fa < fb) {
+                        return -1
+                    }
+                    if (fa > fb) {
+                        return 1
+                    }
+                    return 0
+                } // sort by price
+                else if (this.sortBy == 'price') {
+                    if (a.price > b.price)
+                        return 1;
+                    if (a.price < b.price)
+                        return -1;
+                    return 0;
+                }// sort by availability
+                else if (this.sortBy == 'availability') {
+                    if (a.availableSpace > b.availableSpace)
+                        return 1;
+                    if (a.availableSpace < b.availableSpace)
+                        return -1;
+                    return 0;
+                }
+            }) //sort to asc/desc
+            if (!this.ascending) {
+                subjectsArray.reverse()
+            }
+            return subjectsArray
+        }
     }
 });
