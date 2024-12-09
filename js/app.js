b8cc207723ba3cf1e0b92c6f6728c491b7b3d223
NicoleJhone <105268456+NicoleJhone@users.noreply.github.com>
2024-12-09 19:34:41 +0400

    added add to cart functionality for each lessons

--- js/app.js
+++ js/app.js
@@ -1,56 +1,85 @@
 var webstore = new Vue({
-    el: '#app',
-    data: {
-        sortBy: 'subject',
-        ascending: true,
-        sitename: 'After School Classes',
-        subjects: subjects,
+  el: "#app",
+  data: {
+    sitename: "After School Classes",
+    subjects: subjects,
+    showsubject: true,
+    sortBy: "subject",
+    ascending: true,
+    searchValue: "",
+    cart: [],
+  },
+  methods: {
+    // add to cart button
+    addToCart(subject) {
+      this.cart.push(subject);
     },
-    computed: {
-        sortedSubjects() {
-            let subjectsArray = this.subjects;
+    //if condition for ATC button
+    canAddToCart(subject) {
+      return subject.availableSpace > this.cartCount(subject);
+    },
 
-            // sort by subjects
-            subjectsArray = subjectsArray.sort((a, b) => {
-                if (this.sortBy == 'subject') {
-                    let fa = a.title.toLowerCase(), fb = b.title.toLowerCase()
-                    if (fa < fb) {
-                        return -1
-                    }
-                    if (fa > fb) {
-                        return 1
-                    }
-                    return 0
-                } //sort by location
-                else if (this.sortBy == 'location') {
-                    let fa = a.location.toLowerCase(), fb = b.location.toLowerCase()
-                    if (fa < fb) {
-                        return -1
-                    }
-                    if (fa > fb) {
-                        return 1
-                    }
-                    return 0
-                } // sort by price
-                else if (this.sortBy == 'price') {
-                    if (a.price > b.price)
-                        return 1;
-                    if (a.price < b.price)
-                        return -1;
-                    return 0;
-                }// sort by availability
-                else if (this.sortBy == 'availability') {
-                    if (a.availableSpace > b.availableSpace)
-                        return 1;
-                    if (a.availableSpace < b.availableSpace)
-                        return -1;
-                    return 0;
-                }
-            }) //sort to asc/desc
-            if (!this.ascending) {
-                subjectsArray.reverse()
-            }
-            return subjectsArray
+    // checkout/lesson toggle
+    showCart() {
+      this.showsubject = !this.showsubject;
+    },
+    // cart length count
+    cartCount(id) {
+      let count = 0;
+      for (let i = 0; i < this.cart.length; i++) {
+        if (this.cart[i] === id) {
+          count++;
         }
+      }
+      return count;
     }
+  },
+  computed: {
+    sortedSubjects() {
+      let subjectsArray = this.subjects;
+
+      // sort by subjects
+      subjectsArray = subjectsArray.sort((a, b) => {
+        if (this.sortBy == "subject") {
+          let fa = a.title.toLowerCase(),
+            fb = b.title.toLowerCase();
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        } //sort by location
+        else if (this.sortBy == "location") {
+          let fa = a.location.toLowerCase(),
+            fb = b.location.toLowerCase();
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        } // sort by price
+        else if (this.sortBy == "price") {
+          if (a.price > b.price) return 1;
+          if (a.price < b.price) return -1;
+          return 0;
+        } // sort by availability
+        else if (this.sortBy == "availability") {
+          if (a.availableSpace > b.availableSpace) return 1;
+          if (a.availableSpace < b.availableSpace) return -1;
+          return 0;
+        }
+      }); //sort to asc/desc
+      if (!this.ascending) {
+        subjectsArray.reverse();
+      }
+      return subjectsArray;
+    },
+    cartItemCount: function () {
+      return this.cart.length;
+    },
+  },
 });
